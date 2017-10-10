a465e61870cf6f94372fe7db603e2ccb554fc5fa
Aline Bastos <alineabp@gmail.com>
2017-10-10 13:36:47 -0300

    Creating de Container component

---
+++ client/src/components/Container.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const Container = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  @media (max-width: 1000px) {
+    padding: 0 10px;
+  }
+`;
+
+export default Container;
