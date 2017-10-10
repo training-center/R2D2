a140bb158dfa4fabca5881b19406a1e7a8109b6c
Aline Bastos <alineabp@gmail.com>
2017-10-09 21:49:37 -0300

    Creating Header styles. Desktop and Mobile.

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -1,14 +1,31 @@
 /* eslint-disable max-len */
 import React from 'react'
 import Header from '../components/Header'
+import styled from 'styled-components';
 
 export default function Home() {
+
+const Wrapper = styled.section`
+  margin: 0;
+  height: 100%;
+`;
+
+const Container = styled.div`
+  max-width: 1000px;
+  margin: 0 auto;
+  @media (max-width: 1000px) {
+    padding: 0 10px;
+  }
+`;
+
   return (
-    <div className="container">
+    <Wrapper>
       <Header />
       <main>
-        Home
+        <Container>
+          Home
+        </Container>
       </main>
-    </div>
+    </Wrapper>
   )
 }
