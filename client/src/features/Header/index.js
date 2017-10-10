88fd24814638a2e3c9bffe974c60498fb970dcef
Aline Bastos <alineabp@gmail.com>
2017-10-10 15:44:19 -0300

    Fixing width and flexbox styles

--- client/src/features/Header/index.js
+++ client/src/features/Header/index.js
@@ -5,10 +5,14 @@ import logo from 'client/assets/images/logo-tc.png';
 
 const Head = styled.header`
   align-items: center;
+  justify-content: space-between;
   display: flex;
-  padding: 10px;
+  padding: 10px 30px;
   background-color: #f4f4f5;
   box-shadow: 0 0 5px 0 #999;
+  @media (max-width: 1070px) {
+    padding: 10px;
+  }
   @media (max-width: 800px) {
     height: 70px;
   }
@@ -16,7 +20,7 @@ const Head = styled.header`
 
 const Logo = styled(Link)`
   width: 70px;
-  height: 70px;
+  height: auto;
   margin: 0;
 `;
 
@@ -25,7 +29,11 @@ const StyledImg = styled.img`
 `;
 
 const Nav = styled.nav`
-  margin-left: auto;
+  
+`;
+
+const SocialNav = styled.nav`
+  
 `;
 
 const StyledLink = styled(Link)`
@@ -36,6 +44,7 @@ const StyledLink = styled(Link)`
   line-height: 20px;
   margin-left: 20px;
   text-decoration: none;
+  text-shadow: 1px 1px 1px rgba(255, 255, 255, 1);
   &:hover {
     color: #ed684e;
   }
@@ -50,6 +59,10 @@ const Header = () => (
       <StyledLink to="/sign-in">Cadastre-se</StyledLink>
       <StyledLink to="/sign-up">Login</StyledLink>
     </Nav>
+    <SocialNav>
+      <i className="fa fa-github" aria-hidden="true"></i>
+      <StyledLink to="/sign-up">Login</StyledLink>
+    </SocialNav>
   </Head>
 );
 
