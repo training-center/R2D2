3619f3f74a89d6b140cff95256d82c2a6c79eb08
Fernando Fleury Gil <fernandofleuryg@gmail.com>
2017-10-15 14:13:15 +0200

    add css globals to storybook

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,9 +1,22 @@
 import { configure } from '@storybook/react';
+import { injectGlobal } from 'styled-components';
 
 const req = require.context('../src/', true, /story\.js$/);
 
 function loadStories() {
   req.keys().forEach(req);
 }
+
+injectGlobal`
+html {
+  box-sizing: border-box;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+*, *::before, *::after {
+  box-sizing: inherit;
+}
+`
+
 
 configure(loadStories, module);
